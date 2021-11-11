a832db56d2610a70755c848a4a05989086b16d72
amirho1 <amirhosseinsalighedar@gmail.com>
2021-11-11 13:47:32 +0330

    add initial dependencies for puls

--- src/shapes/Puls.test.tsx
+++ src/shapes/Puls.test.tsx
@@ -4,12 +4,17 @@ import Puls from "./Puls";
 
 describe("Puls", () => {
   let element: HTMLElement;
+  let childrenTextContext = "Hello world";
+  let children = <p className="children">{childrenTextContext}</p>;
+  let cursorTestClassName = "cursor-test-class-name";
+
+  // do some initialization before each test
   beforeEach(() => {
     // clean the dom
     cleanup();
 
     //render the Component
-    render(<Puls />);
+    render(<Puls children={children} cursorClassName={cursorTestClassName} />);
 
     // get the element
     element = screen.getByTestId("Puls");
@@ -17,5 +22,22 @@ describe("Puls", () => {
 
   it("should mount on document", () => {
     expect(element).toBeInTheDocument();
+  });
+
+  it("render the given children", () => {
+    expect(element.querySelector(".children")).toBeInTheDocument();
+    expect(element.querySelector(".children")?.textContent).toMatch(
+      childrenTextContext
+    );
+  });
+
+  it("should get puls-cursor-border element", () => {
+    const pulsCursor = element.querySelector(".puls-cursor");
+    expect(pulsCursor).toBeInTheDocument();
+  });
+
+  it("should use given cursorClassName", () => {
+    const pulsCursor = element.querySelector(`.${cursorTestClassName}`);
+    expect(pulsCursor).toBeInTheDocument();
   });
 });
