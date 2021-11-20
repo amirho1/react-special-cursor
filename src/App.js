77497af257a289c952c2543366c91778e3b4681c
amirho1 <amirhosseinsalighedar@gmail.com>
2021-11-20 09:28:08 +0330

    update page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import "./App.css";
 import Cursor from "react-special-cursor";
-
 const welcome = (
   <h3 className="h3">
-    Welcome to Demo of React-special-cursor from version 1.1.0 u will have the
-    ability to show texts, elements ... on hover inside the cursor-dot by only
-    adding a cursorChildren to hoverClass object
-    <span style={{ color: "red" }}> Click to see documentation</span>
     <img
       className="react-icon"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
       alt="react img "
     />
+    Welcome to Demo of React-special-cursor from version 1.1.0 u will have the
+    ability to show texts, elements ... on hover inside the cursor-dot by only
+    adding a cursorChildren to hoverClass object
+    <span style={{ color: "red" }}> Click to see documentation</span>
   </h3>
 );
 
 export default function App() {
   return (
     <Cursor
+      turnOffOnPhone={false}
       hoverClasses={[
         { classNameOfTargetElement: "head", classNameOfStyle: "head-hover" },
         { classNameOfTargetElement: "link", classNameOfStyle: "hover" },
@@ -37,27 +37,37 @@ export default function App() {
           <a
             className="head"
             href="https://github.com/amirho1/react-cursors.git"
-            target="_blank">
+            target="_blank"
+            rel="noreferrer">
             React-cursors
           </a>
         </h1>
         <p>
           A custom animated cursor for{" "}
-          <a className="react" target="_blank" href="https://reactjs.org/">
+          <a
+            className="react"
+            target="_blank"
+            href="https://reactjs.org/"
+            rel="noreferrer">
             React
           </a>{" "}
           also supports{" "}
           <a
             className="link"
             target="_blank"
-            href="https://www.typescriptlang.org/download">
+            href="https://www.typescriptlang.org/download"
+            rel="noreferrer">
             {" "}
             Typescript
           </a>
         </p>
         <p>You can hover over the links and click to see animations</p>
         Author:{" "}
-        <a href="https://github.com/amirho1" target="_blank" className="author">
+        <a
+          href="https://github.com/amirho1"
+          target="_blank"
+          className="author"
+          rel="noreferrer">
           {" "}
           AmirHossein Salighedar
         </a>
